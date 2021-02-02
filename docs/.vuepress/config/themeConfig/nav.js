afc3797fed84e5c262d7abdb7c841f020e51b82d
wangrunyuanj <wangrunyuanj@163.com>
2021-02-02 08:46:36 +0800

    'init'

---
+++ docs/.vuepress/config/themeConfig/nav.js
@@ -0,0 +1,21 @@
+// nav
+module.exports = [
+  { text: '首页', link: '/' },
+  {
+    text: 'demo',
+    link: '/pages/273213/'
+  },
+  {
+    text: '收藏夹',
+    link: '/pages/beb6c0bd8a66cea6'
+  },
+  {
+    text: '索引',
+    link: '/archives/',
+    items: [
+      { text: '分类', link: '/categories/' },
+      { text: '标签', link: '/tags/' },
+      { text: '归档', link: '/archives/' },
+    ],
+  },
+]
